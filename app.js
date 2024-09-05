78afee4706f3c615a4a612e0aa98f5f5848ff60e
mehul1409 <mehulbansalswm1234@gmail.com>
2024-09-05 18:47:03 +0530

    minor updates

--- app.js
+++ app.js
@@ -9,27 +9,29 @@ const socketio = require('socket.io');
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render("index");
 })
 
-io.on("connection",function(socket){
-    socket.on("send-location",function(data){
-        io.emit("receive-location",{id:socket.id, ...data})
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data })
     });
 
-    socket.on("disconnect",function(){
-        io.emit("user-disconnect",socket.id)
-    })
-})
+    socket.on("disconnect", () => {
+        io.emit("user-disconnect", socket.id);
+        console.log("A user disconnected with id:", socket.id);
+    });
+});
 
-server.listen(5000,(err)=>{
-    if(err){
+
+server.listen(5000, (err) => {
+    if (err) {
         console.log(`error : ${err}`);
-    }else{
+    } else {
         console.log("Server successfully started!");
     }
 });
